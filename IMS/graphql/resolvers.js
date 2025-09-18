c3be2e3994f4f221bb2d35c7709c53ed26c2da9a
Erik-Fullstack <Erik.andersson@chasacademy.se>
2025-09-18 14:00:48 +0200

    bra tv

--- IMS/graphql/resolvers.js
+++ IMS/graphql/resolvers.js
@@ -1,1 +1,11 @@
-export const resolvers = {};
+import { Product } from "../models/product.js";
+import mongoose from "mongoose"
+
+export const resolvers = {
+    Query: {
+        getProduct: async (_parent, args) => {
+            const product = await Product.find()
+            return product
+        }
+    }
+};
